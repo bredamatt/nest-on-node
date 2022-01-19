e19652dcdcef15b3ca1909c3dde8d45480ebd324
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-01-19 12:32:44 +0000

    Create public decorator and add to api key guard

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Inject, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Inject, Param, Patch, Post, Query, SetMetadata, UsePipes, ValidationPipe } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
+import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -17,6 +18,7 @@ export class CoffeesController {
     }
 
     // @UsePipes(ValidationPipe) --> Route specific pipe
+    @Public() // Custom decorator
     @Get()
     findAll(@Query() paginationQuery: PaginationQueryDto) {
         // const { limit, offset } = paginationQuery;
